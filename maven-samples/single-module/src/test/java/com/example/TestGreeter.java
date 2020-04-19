5803b3b4e11a9d54a19a167a1a014a351e13eb40
multani <mehtabmultani51@gmail.com>
2020-04-19 09:52:40 +0530

    change in testgreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -39,7 +39,7 @@ public class TestGreeter {
   }
 
     @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingMsg() {
     String someone = "World, stay home stay safe";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
